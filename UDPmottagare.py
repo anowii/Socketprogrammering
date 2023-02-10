5ec33604912159ceb3fbd523bcfb8bbebc4c8485
anowii <91365639+anowii@users.noreply.github.com>
2023-02-10 14:07:22 +0100

    Update UDPmottagare.py

--- UDPmottagare.py
+++ UDPmottagare.py
@@ -1,27 +1,33 @@
-# UDP Mottagare
+# TCP Mottagare
 from socket import *
 serverPort = 12000
+lastNum = 10000
 
-# create UDP socket and bind to specified port
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
+# create TCP welcoming socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('',serverPort))
 
-print ("The UDP server is ready to recieve")
-lastNum = 10000 
+# server starts listening for incoming TCP requests
+serverSocket.listen(1)
+
+print ('The TCP server is ready to receive')
+# server waits for incoming requests; new socket created on return
+connectionSocket, addr = serverSocket.accept()
 while True:
-    # read client's message and remember client's address (IP and port)
-    message, clientAddress = serverSocket.recvfrom(2048)
-
-    payload = message.decode()
+    # read payload of bytes from socket sent by the client
+    payload = connectionSocket.recv(1500).decode()
     msgNum = int(payload[:5])
-    # Print packet nr
+    # print package number 
     print ("Packet nr: ", msgNum)
-    
-    lastNum2 = msgNum - 1
+
     # if the (current packaged number -1) != (the last package number) the packages are in disorder.
-    if(lastNum != lastNum2):
+    lastNum2 = msgNum - 1
+    if(lastNum != lastNum2 and msgNum != 0):
         print ("log: felaktig ordning", lastNum, "=>", msgNum)
     # saves the current package number
     lastNum = int(payload[:5])    
-    #Print client address 
-    print (clientAddress)
+    # print client address
+    print (addr)
+
+# close the TCP connection; the welcoming socket continues
+connectionSocket.close()
